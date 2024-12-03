54866e33db46067f6f29315ac1affda5e21de471
andreapanico12 <andreapanico12@gmail.com>
2024-12-03 15:57:58 +0100

    feat: add base exercise layout and define variables in script

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+
+
+
+// Definizione delle variabili
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+let emails = [];
+let counterMails = 0;
+
+const ulEmails = document.getElementById('mail-list');
+const mailsButton = document.getElementById('btn-gen');
+
+console.log(endpoint)
+
+
